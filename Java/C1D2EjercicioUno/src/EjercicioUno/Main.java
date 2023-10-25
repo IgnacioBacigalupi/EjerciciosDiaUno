94856da101993405ac42248fa29061a5f0cb80b8
Ignacio Bacigalupi <120676527+IgnacioBacigalupi@users.noreply.github.com>
2023-10-25 11:14:16 +0200

    Add files via upload

---
+++ Java/C1D2EjercicioUno/src/EjercicioUno/Main.java
@@ -0,0 +1,16 @@
+package EjercicioUno;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hola bienvenido al Bootcamp");// Mostramos por pantalla el mensaje
+
+		String nombre = "Nacho"; // Creamos e inicializamos la variable nombre
+
+		System.out.println("Hola " + nombre + " , bienvenido al bootcamp."); // Mostramos de nuevo el mensaje con la
+																				// variable nombre concatenada
+
+
+	}
+
+}
