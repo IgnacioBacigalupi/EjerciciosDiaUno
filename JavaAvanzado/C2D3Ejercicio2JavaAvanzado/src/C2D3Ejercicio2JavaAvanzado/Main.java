9bec51fab10346afeae5a75f1ff70b1cd999b241
Ignacio Bacigalupi <120676527+IgnacioBacigalupi@users.noreply.github.com>
2023-11-09 12:42:03 +0100

    Add files via upload

---
+++ JavaAvanzado/C2D3Ejercicio2JavaAvanzado/src/C2D3Ejercicio2JavaAvanzado/Main.java
@@ -0,0 +1,19 @@
+package C2D3Ejercicio2JavaAvanzado;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		int numTrajadores = 5; // Número de trabajadores en la fábrica
+
+        for (int i = 1; i <= numTrajadores; i++) {
+            Trabajador trabajador = new Trabajador(i);
+            Thread thread = new Thread(trabajador);
+            Thread.sleep(1000);
+            thread.start();
+           // thread.join();
+            System.out.println("------------------------");
+        }
+	}
+
+}
