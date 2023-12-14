f3bb138ed0e160025867f01d6c27fe8efdedfaa9
Ignacio Bacigalupi <120676527+IgnacioBacigalupi@users.noreply.github.com>
2023-12-14 11:24:53 +0100

    Add files via upload

---
+++ SpringBoot/Clase1Dia2Ejercicio1/src/main/java/Clase1Dia2/Clase1Dia2/Clase1Dia2Application.java
@@ -0,0 +1,13 @@
+package Clase1Dia2.Clase1Dia2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase1Dia2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase1Dia2Application.class, args);
+	}
+
+}
