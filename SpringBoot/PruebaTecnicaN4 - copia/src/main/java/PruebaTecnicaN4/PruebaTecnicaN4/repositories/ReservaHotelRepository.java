4217758a19f1a27d73b270dd4ba4ea8c07f28b3a
Ignacio Bacigalupi <120676527+IgnacioBacigalupi@users.noreply.github.com>
2024-01-09 22:26:51 +0100

    Delete SpringBoot/PruebaTecnicaN4 - copia directory

--- SpringBoot/PruebaTecnicaN4 - copia/src/main/java/PruebaTecnicaN4/PruebaTecnicaN4/repositories/ReservaHotelRepository.java
+++
@@ -1,9 +0,0 @@
-package PruebaTecnicaN4.PruebaTecnicaN4.repositories;
-
-import PruebaTecnicaN4.PruebaTecnicaN4.model.ReservaHotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservaHotelRepository extends JpaRepository<ReservaHotel , Long> {
-}
